2db48acfc3ddce0c8fc572b5ef18083bbc740a9e
Ruba M. Alsmail <ruba.mm.ss@gmail.com>
2021-02-02 10:46:17 +0300

    dashboard update + little changes

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,24 @@
+/*importScripts('https://www.gstatic.com/firebasejs/7.21.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-messaging.js');
+
+importScripts('https://www.gstatic.com/firebasejs/8.2.3/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.2.3/firebase-messaging.js');
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBDUVxL_S0b9AxSRjBsQ6ri80mhO0kZ03c",
+    authDomain: "mc-mazaya.firebaseapp.com",
+    databaseURL: "https://mc-mazaya.firebaseio.com",
+    projectId: "mc-mazaya",
+    storageBucket: "mc-mazaya.appspot.com",
+    messagingSenderId: "1043747982646",
+    appId: "1:1043747982646:web:eb8806eb17fa88668fb797",
+    measurementId: "G-5VCPWL0HQL"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+  
+  const auth=firebase.auth();
+
+
+
+  const messaging = firebase.messaging();*/
--- manageTradeMarkes/Notification.js
+++ manageTradeMarkes/Notification.js
@@ -8,7 +8,23 @@ const firebaseConfig = {
   appId: "1:1043747982646:web:eb8806eb17fa88668fb797",
   measurementId: "G-5VCPWL0HQL"
 };
-//var GOOGLE_APPLICATION_CREDENTIALS="/home/user/Downloads/service-account-file.json"
+/*---
+curl -i -H 'Content-type: application/json' -H 'Authorization: key=<AIzaSyBDUVxL_S0b9AxSRjBsQ6ri80mhO0kZ03c>' -XPOST https://fcm.googleapis.com/fcm/send -d '{
+  "registration_ids":["registration_ids", "of the", "target", "devices as array"],
+  "notification": {
+      "title":"Title of your notification -Ruba Test",
+      "body":"content of your notification- Test Test"
+  },
+  "data": {
+    "key1" : "value1",
+    "key2" : "value2",
+    "key3" : 23.56565,
+    "key4" : true
+  }
+}'
+
+curl --header "Content-Type: application/json" \ --header "Authorization: key="AAAA8wQ6TTY:APA91bE6UQsP1kDeLqnBMvqQsQTQOkRwS9rLdDlZU-TwiDvnJPmVOKqxlz1QeHaIiKkKOs5baMsC-2sNNTvXJ3xFfgUfYwcdOPLFa8MU-S1fKd_iUWuiIyNe_Cy7R6ndufy222LvN5nF" https://fcm.googleapis.com/fcm/send \ -d '{"data": {"title": "The Title","body": "Hellof!", "sound": "default"}, "priority": "high", "to": "FMC push token"}'
+//---*/
 
 firebase.initializeApp(firebaseConfig);
 
@@ -26,3 +42,9 @@ const auth=firebase.auth();
   .catch(function(err){
     console.log('Error occured');
   })
+
+
+  //for the web notification
+  messaging.onMessage(function(payload){
+    console.log('onMessage: '+ payload);
+  });
