d95d9d113d25f89991362a398957117534df7a60
Ruba M. Alsmail <ruba.mm.ss@gmail.com>
2021-03-02 13:46:16 +0300

    -

--- manageTradeMarkes/testbackend/index.js
+++ manageTradeMarkes/testbackend/index.js
@@ -88,78 +88,97 @@
 
 
 
-const express = require('express')
-const app = express();
-const port = 3000
+// const express = require('express')
+// const app = express();
+// const port = 3000
 
-var admin = require('firebase-admin');
-app.engine("html", require("ejs").renderFile);
+// var admin = require('firebase-admin');
+// app.engine("html", require("ejs").renderFile);
 
-app.use(bodyParser.json());
-app.use(csrfMiddleware);
-
-
-admin.initializeApp({
-         projectId: "mc-mazaya",
-         credential: admin.credential.applicationDefault(),
-          databaseURL: 'https://<mc-mazaya>.firebaseio.com',
-  });
+// app.use(bodyParser.json());
+// app.use(csrfMiddleware);
 
 
+// admin.initializeApp({
+//          projectId: "mc-mazaya",
+//          credential: admin.credential.applicationDefault(),
+//           databaseURL: 'https://<mc-mazaya>.firebaseio.com',
+//   });
 
-app.get("/login", function (req, res) {
-  res.render("login.html");
-});
 
-app.get("/signup", function (req, res) {
-  res.render("signup.html");
-});
 
-app.get("/profile", function (req, res) {
- // const sessionCookie = req.cookies.session || "";
+// app.get("/login", function (req, res) {
+//   res.render("login.html");
+// });
 
-  var email='amutairi@mc.gov.sa';
-      admin
-      .auth()
-      .getUserByEmail(email)
-      .then((userRecord) => {
-        // See the UserRecord reference doc for the contents of userRecord.
-        console.log(`Successfully fetched user data: ${userRecord.toJSON()}`);
-        res.send(userRecord.toJSON())
-      //  res.send('hi! and bye')
+// app.get("/signup", function (req, res) {
+//   res.render("signup.html");
+// });
+
+// app.get("/profile", function (req, res) {
+//  // const sessionCookie = req.cookies.session || "";
+
+//   var email='amutairi@mc.gov.sa';
+//       admin
+//       .auth()
+//       .getUserByEmail(email)
+//       .then((userRecord) => {
+//         // See the UserRecord reference doc for the contents of userRecord.
+//         console.log(`Successfully fetched user data: ${userRecord.toJSON()}`);
+//         res.send(userRecord.toJSON())
+//       //  res.send('hi! and bye')
   
-      })
-      .catch((error) => {
-        console.log('Error fetching user data:', error);
-        res.send('Error!')
-      });
-});
+//       })
+//       .catch((error) => {
+//         console.log('Error fetching user data:', error);
+//         res.send('Error!')
+//       });
+// });
 
-app.get("/", function (req, res) {
-  res.redirect("/Users/raghad/Documents/GitHub/MazayaAdmin/manageTradeMarkes/manageTradeMarksHome.html");
-});
+// app.get("/", function (req, res) {
+//   res.redirect("/Users/raghad/Documents/GitHub/MazayaAdmin/manageTradeMarkes/manageTradeMarksHome.html");
+// });
 
-app.post("/sessionLogin", (req, res) => {
-res.send('hiii')
-});
+// app.post("/sessionLogin", (req, res) => {
+// res.send('hiii')
+// });
 
-app.get("/sessionLogout", (req, res) => {
-  res.redirect("/Login");
-});
+// app.get("/sessionLogout", (req, res) => {
+//   res.redirect("/Login");
+// });
 
 
-app.post('/submit-student-data', function (req, res) {
-    var name = req.body.firstName + ' ' + req.body.lastName;
+// app.post('/submit-student-data', function (req, res) {
+//     var name = req.body.firstName + ' ' + req.body.lastName;
     
-    res.send(name + ' Submitted Successfully!');
+//     res.send(name + ' Submitted Successfully!');
+// });
+
+
+
+// app.listen(port, function(error)  {
+// if (error){
+//     console.log(`there is an error`)
+// }
+// else{
+//   console.log(`Example app listening at http://localhost:${port}`)}
+// })
+
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+
+
+
+
+
+
+
+
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase!");
 });
-
-
-
-app.listen(port, function(error)  {
-if (error){
-    console.log(`there is an error`)
-}
-else{
-  console.log(`Example app listening at http://localhost:${port}`)}
-})
